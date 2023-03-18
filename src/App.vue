388e4a399ef16d4b9b760888921308e9b4e313ec
Richard <ricky.cary1@gmail.com>
2023-03-18 11:35:28 -0500

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,95 @@
+<!-- Javascript -->
+<script setup>
+import { ref, onBeforeMount } from 'vue';
+import Form from "./components/Form.vue"
+
+const url = "https://capstonepractice-backend.onrender.com/todo"
+const todos = ref([])
+const showForm = ref(false)
+const reminder = ref("")
+const completed = ref(false)
+const _id = ref(null)
+const action = ref("create")
+
+const getTodos = async function() {
+  const response = await fetch(url)
+  const data = await response.json()
+  todos.value = data
+}
+
+const toggleForm = () => {
+  showForm.value = !showForm.value
+}
+
+function resetForm(){
+  _id.value = ""
+  reminder.value = ""
+  completed.value = false
+  action.value = "create"
+}
+
+const createTodo = async (newTodo) => {
+  await fetch(url, {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(newTodo)
+  })
+  getTodos()
+  toggleForm()
+  resetForm()
+}
+
+const updateTodo = async (todo) => {
+  await fetch(url + `/${todo._id}`, {
+    method: "put",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(todo)
+  })
+
+  getTodos()
+  toggleForm()
+  resetForm()
+}
+
+const deleteTodo = async (todo) => {
+  await fetch(url + `/${todo._id}`, {
+    method: "delete"
+  })
+  getTodos()
+}
+
+const selectTodoToEdit = (todo) => {
+  _id.value = todo._id
+  reminder.value = todo.reminder
+  completed.value = todo.completed
+  action.value = "update"
+  toggleForm()
+}
+
+onBeforeMount(() => {getTodos()})
+</script>
+
+
+<!-- HTML -->
+<template>
+  <div>
+    <h1>Ricky's Todo App</h1>
+    <button @click="toggleForm">Create a Todo</button>
+    <ul>
+      <li v-for="todo of todos" v-bind:key="todo._id">
+        <h1 @click="selectTodoToEdit(todo)">{{ todo.reminder }} - {{ todo.completed }}</h1>
+        <button @click="deleteTodo(todo)">Delete</button>
+      </li>
+    </ul>
+    <Form v-if="showForm" v-bind:reminder="reminder" v-bind:completed="completed" v-bind:_id="_id" v-bind:action="action" v-bind:createTodo="createTodo" v-bind:updateTodo="updateTodo" />
+  </div>
+</template>
+
+
+
+<style scoped>
+</style>
---
+++ src/components/Form.vue
@@ -0,0 +1,46 @@
+<!-- Javascript -->
+<script setup>
+import { ref, defineProps } from 'vue';
+
+const reminder = ref(props.reminder)
+const completed = ref(props.completed)
+const _id = ref(props._id)
+const action = ref(props.action)
+const props = defineProps({
+    reminder: String,
+    completed: Boolean,
+    _id: String,
+    action: String,
+    createTodo: Function,
+    updateTodo: Function
+})
+
+const handleSubmit = () => {
+    if (action.value === "create"){
+        const todo = {reminder: reminder.value, completed: completed.value}
+        props.createTodo(todo)
+    } if (action.value === "update") {
+        const todo = {reminder: reminder.value, completed: completed.value, _id: _id.value}
+        props.updateTodo(todo)
+    }
+}
+
+console.log(props)
+</script>
+
+
+<!-- HTML -->
+<template>
+  <div>
+    <form @submit="handleSubmit">
+        <input type=text v-model="reminder">
+        <input type=checkbox v-model="completed">
+        <input type=submit value=submit>
+    </form>
+  </div>
+</template>
+
+
+
+<style scoped>
+</style>
